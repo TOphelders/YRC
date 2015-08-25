f7c902e7343c849ccd570fc007c227b636411e4e
Trevor Ophelders <t.ophelders@gmail.com>
2015-08-25 16:20:30 -0400

    Update IDList class to be more generic, add messaging actions

--- public/jsx/base.js
+++ public/jsx/base.js
@@ -3,53 +3,89 @@ import { Dispatcher } from 'flux';
 
 export class IDList {
   /*
-   * A class to store items to be displayed,
+   * A class to store items to be displayed;
    * allows for O(1) retrieval and modification
-   * by both index and message id
+   * by both index and id
    */
-  constructor() {
-    this._messages = [];
+  constructor(id_prop, list) {
+    this._id_prop = id_prop;
+    this._list = [];
     this._ids = {};
+
+    if (typeof list === 'undefined') return;
+    for (let item of list) {
+      this._ids[item[id_prop]] = this._list.length;
+      this._list.push(item);
+    }
   }
 
   retrieve(id) {
     let index = (typeof id === 'string') ? this._ids[id] : id;
-    return this._messages[index];
+    return this._list[index];
   }
 
-  push(message) {
-    this._ids[message.message_id] = this._messages.length;
-    this._messages.push(message);
+  retrieve_all() {
+    return this._list;
+  }
+
+  push(item) {
+    this._ids[item[this._id_prop]] = this._list.length;
+    this._list.push(item);
   }
 
   modify(id, update) {
     let index = (typeof id === 'string') ? this._ids[id] : id;
-    this._messages[index] = update;
+    if(!index || index >= this.length || index < 0) return;
+    this._list[index] = update;
   }
 
   remove(id) {
-    let index = this._ids[id];
-    this._messages.splice(index, 1);
+    let index;
+    if(typeof id === 'string') {
+      index = this._ids[id];
+      if(!index) return;
+    } else {
+      if(!this._list[id]) return;
+      [id, index] = [this._list[id][this._id_prop], id];
+    }
+
+    this._list.splice(index, 1);
     delete this._ids[id];
 
-    for (let i = index; i < this._messages.length; i++) {
-      let cur = this._messages[i].message_id;
+    for (let i = index; i < this._list.length; i++) {
+      let cur = this._list[i][this._id_prop];
       this._ids[cur]--;
     }
   }
 
+  concat(other) {
+    let list = new IDList(this._list);
+    for(let item of other) list.push(item);
+    return list;
+  }
+
   get length() {
-    return this._messages.length;
+    return this._list.length;
   }
 
   [Symbol.iterator]() {
     let index = -1;
     return {
-      next: () => ({value: this._messages[++index],
-                    done: index == this._messages.length})
+      next: () => ({value: this._list[++index],
+                    done: index == this._list.length})
     };
   }
 };
 
-export let appDispatcher = new Dispatcher();
 export { default as React } from 'react';
+export let AppDispatcher = new Dispatcher();
+export let MessageActions = {
+  RETRIEVE_SET: 'message-retrieve-set',   // A set of messages gotten
+  SEND: 'message-send',                   // Message added by user
+  POSTED: 'message-posted',               // Message successfully sent
+  RETRIEVE: 'message-retrieve',           // A single message gotten
+  EDITED: 'message-edited',               // Message successfully edited
+  EDIT: 'message-edit',                   // User attempted to edit message
+  DELETED: 'message-deleted',             // Message successfully deleted
+  DELETE: 'message-delete'                // Message marked for deletion
+};
